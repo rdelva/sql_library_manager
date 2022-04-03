ba129e0af67f7144f2c09c53486603251fe94bad
rdelva <roseldelva@gmail.com>
2022-04-02 21:35:55 -0400

    inital commit and added index.js to set up Book database

---
+++ db/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+   dialect: 'sqlite',
+   storage:'books.db',
+   logging: false
+});
+
+const db = {
+    sequelize,
+    Sequelize,
+    models:{},
+}
+
+db.models.Book = require('./models/book.js')(sequelize);
