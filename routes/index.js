a7ce7c8c8c04adae0b275bed4ed3f8cdb6728988
rdelva <roseldelva@gmail.com>
2022-04-11 21:46:58 -0400

    making corrections of paths and file locations

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET All the books listing. */
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Books.findAll();
+  console.log(books);
+  //res.render("index", { Books, title: "Sequelize-It!" });
+}));
+
+
+
 
 module.exports = router;
