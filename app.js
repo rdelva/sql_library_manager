c8c776f0160e2fe65f2a8de67b3271272ef240d4
rdelva <roseldelva@gmail.com>
2022-04-12 00:26:17 -0400

    hide user router

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 const sequelize = require('./models').sequelize; // import Sequelize
 
